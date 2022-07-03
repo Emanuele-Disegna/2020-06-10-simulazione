ede207b865aef9d67a423fccd16a5361161e057f
admin <admin@PC-di-Ema>
2022-07-03 17:35:33 +0200

    Solo punto 1

--- src/main/java/it/polito/tdp/imdb/db/ImdbDAO.java
+++ src/main/java/it/polito/tdp/imdb/db/ImdbDAO.java
@@ -6,7 +6,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+
 import it.polito.tdp.imdb.model.Actor;
+import it.polito.tdp.imdb.model.Adiacenza;
 import it.polito.tdp.imdb.model.Director;
 import it.polito.tdp.imdb.model.Movie;
 
@@ -84,9 +87,80 @@ public class ImdbDAO {
 		}
 	}
 	
+	public void getVertici(String genere, Map<Integer, Actor> idMap){
+		String sql = "SELECT distinct a.*, m.name "
+				+ "FROM movies_genres mg, movies m, roles r, actors a "
+				+ "WHERE mg.movie_id=m.id AND m.id=r.movie_id AND a.id=r.actor_id AND mg.genre=?";
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, genere);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				Actor actor = new Actor(res.getInt("id"), res.getString("first_name"), res.getString("last_name"),
+						res.getString("gender"));
+				idMap.put(res.getInt("id"), actor);
+			}
+			conn.close();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	
-	
+	public List<Adiacenza> getArchi(String genere, Map<Integer, Actor> idMap){
+		String sql = "SELECT t1.id AS id1, t2.id AS id2, COUNT(*) AS peso "
+				+ "FROM (SELECT distinct a.id, m.name "
+				+ "FROM movies_genres mg, movies m, roles r, actors a "
+				+ "WHERE mg.movie_id=m.id AND m.id=r.movie_id AND a.id=r.actor_id AND mg.genre=? "
+				+ ") AS t1, (SELECT distinct a.id, m.name "
+				+ "FROM movies_genres mg, movies m, roles r, actors a "
+				+ "WHERE mg.movie_id=m.id AND m.id=r.movie_id AND a.id=r.actor_id AND mg.genre=? "
+				+ ") AS t2 "
+				+ "WHERE t1.id>t2.id AND t1.name=t2.name "
+				+ "GROUP BY id1, id2 ";
+				
+		List<Adiacenza> result = new ArrayList<>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, genere);
+			st.setString(2, genere);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {
+				result.add(new Adiacenza(idMap.get(res.getInt("id1")), idMap.get(res.getInt("id2")), 
+									res.getInt("peso")));
+			}
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public List<String> getGeneri() {
+		String sql = "SELECT genre FROM movies_genres";
+		List<String> result = new ArrayList<>();
+		Connection conn = DBConnect.getConnection();
+
+		try {
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet res = st.executeQuery();
+			while (res.next()) {				
+				result.add(res.getString("genre"));
+			}
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 	
 	
 }
--- src/main/java/it/polito/tdp/imdb/model/Model.java
+++ src/main/java/it/polito/tdp/imdb/model/Model.java
@@ -1,5 +1,60 @@
 package it.polito.tdp.imdb.model;
 
-public class Model {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.alg.connectivity.ConnectivityInspector;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.imdb.db.ImdbDAO;
+
+public class Model {
+	private ImdbDAO dao;
+	private Graph<Actor, DefaultWeightedEdge> grafo;
+	private Map<Integer, Actor> idMap;
+	
+	public Model() {
+		dao = new ImdbDAO();
+	}
+	
+	public void creaGrafo(String genere) {
+		grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		
+		idMap = new HashMap<>();
+		dao.getVertici(genere, idMap);
+		
+		Graphs.addAllVertices(grafo, idMap.values());
+		
+		for(Adiacenza a : dao.getArchi(genere, idMap)) {
+			Graphs.addEdgeWithVertices(grafo, a.getA1(), a.getA1(), a.getPeso());
+		}
+		
+		System.out.println(grafo.vertexSet().size());
+		System.out.println(grafo.edgeSet().size());
+	}
+	
+	public List<Actor> getSimili(Actor a) {
+		ConnectivityInspector<Actor, DefaultWeightedEdge> c = new ConnectivityInspector<>(grafo);
+		List<Actor> ret = new ArrayList<>(c.connectedSetOf(a));
+
+		Collections.sort(ret, Comparator.comparing(Actor::getLastName));
+		
+		return ret;
+	}
+	
+	public List<String> getGeneri(){
+		return dao.getGeneri();
+	}
+	
+	public Collection<Actor> getAttori(){
+		return idMap.values();
+	}
 }
